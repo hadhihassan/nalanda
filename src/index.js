35c723ae36b552769731c14ffe5507714882328b
hadhihassan <diluhadhi01@gmail.com>
2025-08-27 03:17:39 -0700

    fix: Server port is changed.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 import { connectDatabase } from "./config/db.config.js";
 
 connectDatabase();
-app.listen(config.port, () => {
+app.listen(config.port,"0.0.0.0", () => {
     console.log(`Server Status: \t\tRunning on http://localhost:${config.port}`);
 });
